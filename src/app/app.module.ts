c41e37be30127b6a37929588e0f69c3a1bf04733
Marcos8060 <marcosgav80@gmail.com>
2022-03-23 01:06:04 +0300

    created the details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { FleetComponent } from './fleet/fleet.component';
+import { DetailsComponent } from './details/details.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+const appRoutes: Routes = [
+  { path: '' , component : HeroComponent },
+  { path: 'fleet', component : FleetComponent},
+  { path: 'details', component: DetailsComponent},
+  { path: 'profile', component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    FleetComponent
+    FleetComponent,
+    DetailsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
